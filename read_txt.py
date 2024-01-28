5d5d1b2195cf4786db694e9e5847443f205cb1fc
nestaraus <nestaraus@mail.pu>
2024-01-28 20:05:11 +0300

    Создал функцию read_txt

---
+++ read_txt.py
@@ -0,0 +1,8 @@
+def read_txt(filename): 
+    phone_book=[]
+    fields=['Фамилия', 'Имя', 'Телефон', 'Описание']
+    with open(filename,'r',encoding='utf-8') as phb:
+        for line in phb:
+            record = dict(zip(fields, line.split(',')))
+            phone_book.append(record)	
+    return phone_book
