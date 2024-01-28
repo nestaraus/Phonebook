27cd0816e3a20808e1c4b21b236919db2fcc07b5
nestaraus <nestaraus@mail.pu>
2024-01-28 20:15:32 +0300

    Создал 2 функции для добавления контакта в справочник

---
+++ get_new_number.py
@@ -0,0 +1,12 @@
+def get_new_number():
+    last_name = input('Введите фамилию: ')
+    first_name = input('Введите имя: ')
+    phone_number = input('Введите номер телефона: ')
+    dop_info = input('Введите дополнительную информацию: ')
+    return last_name, first_name, phone_number, dop_info 
+
+
+def add_phone_number(file_name):
+    info = ','.join(get_new_number())
+    with open(file_name, 'a', encoding='utf-8') as file:
+        file.write(f'{info}\n')
