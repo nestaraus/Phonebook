369c258d37a4fdcd6b01cef8cab5094a9d8d86b2
nestaraus <nestaraus@mail.pu>
2024-01-28 20:13:39 +0300

    Создал функцию для получения результата поиска по имени/фамилии

---
+++ find_contact.py
@@ -0,0 +1,14 @@
+from search_param import*
+from print_result import*
+def find_contact(contact_list):
+    search_field, search_value = search_param()
+    search_value_dict = {'1': 'Фамилия', '2': 'Имя', '3': 'Номер телефона'}
+    found_contacts = []
+    for contact in contact_list:
+        if contact[search_value_dict[search_field]] == search_value:
+            found_contacts.append(contact)
+    if len(found_contacts) == 0:
+        print('Контакт не найден!')
+    else:
+        print_result(found_contacts)
+    print()   
